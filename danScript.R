cbd77045e2dd710b42fbf875c1254bd210ed7a47
Tim Killian <tskillian@gmail.com>
2014-07-18 00:11:32 -0700

    update to take date from command line + new wd

--- danScript.R
+++ danScript.R
@@ -1,42 +1,43 @@
 ## Capital bikeshare data science project
+dateTime <- commandArgs(trailingOnly = TRUE) # passed in as command line argument from operating system
 
 lapply(.packages(all.available = TRUE), function(xx) library(xx,character.only = TRUE))
 
-setwd("C:/Users/Dan/Dropbox/personal/Coursera/data science certificate/data science project")
+setwd("/home/ec2-user/r_scripts/data")
 
 ## Download XML data and transform to data table
-
 fileUrl <- "http://www.capitalbikeshare.com/data/stations/bikeStations.xml"
+library(XML)
 parse <- xmlTreeParse(fileUrl,useInternal=T)
 rootNode <- xmlRoot(parse)  
 dataprep1 <- (rootNode)
 dataprep2 <- xmlSApply(dataprep1,function(x) xmlSApply(x,xmlValue))
-systemdata8June7.30pm <- data.frame(t(dataprep2),row.names=NULL)
+data <- data.frame(t(dataprep2),row.names=NULL)
 
 ## Convert Unix stamp to character, remove last three digits, convert to numeric and render to date
-systemdata8June7.30pm[,15] <- as.character(systemdata8June7.30pm[,15])
-class(systemdata8June7.30pm[,15])
+data[,15] <- as.character(data[,15])
+class(data[,15])
      
-for(i in systemdata8June7.30pm[,15]) {
-  systemdata8June7.30pm$date <- substr(systemdata8June7.30pm[,15], 1, 10)
+for(i in data[,15]) {
+  data$date <- substr(data[,15], 1, 10)
 }
 
-systemdata8June7.30pm[,16] <- as.numeric(systemdata8June7.30pm[,16])
-class(systemdata8June7.30pm[,16])
+data[,16] <- as.numeric(data[,16])
+class(data[,16])
   
-systemdata8June7.30pm[,16] <- as.POSIXct(systemdata8June7.30pm[,16],origin="1970-01-01")
+data[,16] <- as.POSIXct(data[,16],origin="1970-01-01")
 
 ## Compute total of bikes/docks and percentage of each
 
-systemdata8June7.30pm[,13] <- as.numeric(systemdata8June7.30pm[,13])
-systemdata8June7.30pm[,14] <- as.numeric(systemdata8June7.30pm[,14])                                            
-class(systemdata8June7.30pm[,14])
+data[,13] <- as.numeric(data[,13])
+data[,14] <- as.numeric(data[,14])                                            
+class(data[,14])
 
-systemdata8June7.30pm$total <- systemdata8June7.30pm$nbBikes + systemdata8June7.30pm$nbEmptyDocks
+data$total <- data$nbBikes + data$nbEmptyDocks
 
-systemdata8June7.30pm$percentBikes <- systemdata8June7.30pm$nbBikes / systemdata8June7.30pm$total
+data$percentBikes <- data$nbBikes / data$total
 
-systemdata8June7.30pm$percentDocks <- systemdata8June7.30pm$nbEmptyDocks / systemdata8June7.30pm$total
+data$percentDocks <- data$nbEmptyDocks / data$total
 
 ## Save file for later aggregation
-write.csv(systemdata8June7.30pm,file="systemdata 8 June 2014 7.30pm.csv")
+write.csv(data,file=paste(dateTime, ".csv", sep=""))
